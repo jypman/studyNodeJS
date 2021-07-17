bb3cfb5f2c799d645d048c8461f2598e60292779
JINYOUNG PARK <asd029600@gmail.com>
2021-07-17 16:43:48 +0900

    파일명 수정하여 파일 순서 변경

--- 02_1_module.js
+++ 02_1_module.js
@@ -1,8 +1,8 @@
 // 해당 module.exports 변수가 가리키는 함수와 변수에 포함되지 않고 해당모듈 파일에서 발동된 함수를 담을 수 있다.
-const HAHAHA = require('./2_2_module.js');
+const HAHAHA = require('./02_2_module.js');
 
 // 해당 module.exports 변수가 가리키는 함수를 사용하고 싶을 때 여기서 또다른 변수를 선언하여 사용할 수 있다.
-const myWay = require('./2_2_module.js');
+const myWay = require('./02_2_module.js');
 
 // module.exports 변수가 가리키는 함수에 각각 전달인수를 할당한다.
 HAHAHA('jypman');
