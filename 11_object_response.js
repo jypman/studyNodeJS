a041fb41530560994b5368ad05d6b9a8e3563717
JINYOUNG PARK <asd029600@gmail.com>
2021-07-17 16:41:34 +0900

    응답객체 공부

--- 11_object_response.js
+++ 11_object_response.js
@@ -3,8 +3,9 @@ const port = 3000;
 const hostName = '127.0.0.1';
 
 const server = http.createServer((req, res) => {
-    let resData = '<html><body><h1>Hello!!!</h1></body></html>';
-    // 아래 두개의 주석에 있는 코드 대신 그 아래의 한 문장의 간결한 코드로 입력할 수 있다.
+    let resData = '<html lang="ko"><body><h1>Hello!!!</h1></body></html>';
+    // writeHead()의 첫번째 인자에는 응답코드를 의미하며 두번째 인자는 응답헤더를 의미한다.
+    // 아래 두개의 주석에 있는 코드 대신 writeHead()로 간결하게 입력할 수 있다.
     // res.setHeader('Content-type', 'text/plain');
     // res.statusCode = 200;
     res.writeHead(200, {'Content-Type': 'text/html'});
