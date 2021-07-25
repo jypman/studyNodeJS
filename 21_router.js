cbd17859bc9040637fc55160032aeb64a1f01393
JINYOUNG PARK <asd029600@gmail.com>
2021-07-25 16:52:50 +0900

    router 예제 오타 수정

--- 21_router.js
+++ 21_router.js
@@ -7,9 +7,9 @@ const users = require('./21_router_users.js');
 const boards = require('./21_router_boards.js');
 
 //엔드포인트가 /user면 users 모듈 호출
-app.use('./users', users);
+app.use('/users', users);
 //엔드포인트가 /boards면 boards 모듈 호출
-app.use('./boards', boards);
+app.use('/boards', boards);
 
 http.createServer(app).listen(port, () => {
     console.log(`${port}포트에서 서버가 작동하고 있습니다.`)
