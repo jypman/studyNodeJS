d47f94731ed53ab79373222611ff0beeef4ea474
JINYOUNG PARK <asd029600@gmail.com>
2021-10-23 23:17:32 +0900

    sequelize 시드 및 스켈레톤 공부

--- models/user.js
+++ models/user.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
+  }
   User.init({
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
