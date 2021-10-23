2b90a4ad3c01e2c650e05d0be2b95d5536b0fdf7
JINYOUNG PARK <asd029600@gmail.com>
2021-10-24 04:03:49 +0900

    models/index.js에서 디비 정보를 mysql.js에서 불러오는 로직으로 수정

--- config/mysql.js
+++ config/mysql.js
@@ -7,20 +7,47 @@ module.exports = {
         "password": process.env.DATABASE_PASSWORD,
         "database": process.env.DEV_DATABASE_NAME,
         "host": "127.0.0.1",
-        "dialect": "mysql"
+        "dialect": "mysql",
+        // DB 기타 설정
+        "dialectOptions" : {
+            // DB 테이블에 데이터를 넣을 때 이모티콘을 인식할 수 있도록 한다.
+            charset: "utf8mb4",
+            // 날짜를 문자열로 파싱하도록 한다.
+            dateStrings: true,
+            // 타임존을 역으로 계산하지 않는다.
+            typeCast: true
+        },
+        // 한국 타임존으로 설정한다.
+        "timezone" : "+09:00",
+        // sequelize에서 사용한 쿼리문을 콘솔창에서 볼 수 있도록 하는 옵션이며 false는 보지 않는다는 의미
+        "logging" : false
     },
     "test": {
         "username": process.env.DATABASE_USER,
         "password": process.env.DATABASE_PASSWORD,
         "database": process.env.TEST_DATABASE_NAME,
         "host": "127.0.0.1",
-        "dialect": "mysql"
+        "dialect": "mysql",
+        "dialectOptions" : {
+            charset: "utf8mb4",
+            dateStrings: true,
+            typeCast: true
+        },
+        "timezone" : "+09:00",
+        "logging" : false
     },
     "production": {
-        "username": "root",
+        "username": process.env.DATABASE_USER,
         "password": process.env.DATABASE_PASSWORD,
         "database": process.env.PROD_DATABASE_NAME,
         "host": "127.0.0.1",
-        "dialect": "mysql"
+        "dialect": "mysql",
+        "dialectOptions" : {
+            charset: "utf8mb4",
+            dateStrings: true,
+            typeCast: true
+        },
+        "timezone" : "+09:00",
+        "logging" : false
     }
 };
--- models/index.js
+++ models/index.js
@@ -2,35 +2,27 @@
 const Sequelize = require('sequelize');
 const fs = require('fs')
 const path = require('path')
-const dotenv = require('dotenv');
-dotenv.config();
+const basename = path.basename(__filename);
+const config = require(__dirname + '/../config/mysql.js')[process.env.NODE_ENV];
 
-const dbName = process.env.DATABASE_NAME
-const user = process.env.DATABASE_USER
-const password = process.env.DATABASE_PASSWORD
-const options = {
-    host : process.env.DATABASE_HOST,
-    dialect : 'mysql',
-    // 날짜를 문자열로 파싱하도록 한다.
-    dialectOptions: { charset: "utf8mb4", dateStrings: true, typeCast: true },
-    // 한국 타임존으로 설정한다.
-    timezone: "+09:00",
-    // sequelize에서 사용한 쿼리문을 콘솔창에서 볼 수 있도록 하는 옵션이며 false로 보이지 않도록 할 수 있다.
-    logging : false
-}
-
-let sequelize = new Sequelize(dbName, user, password, options)
+let sequelize;
 let db = {};
+
+if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 // 모델 파일들을 조회
 // fs.readdirSync(__dirname)은 해당 경로 내의 파일을 요소로 갖는 배열을 만든다.
 fs.readdirSync(__dirname)
     .filter(file => {
-        // 해당 경로 내의 파일 중에서 .으로 시작하는 파일과 index.js파일은 거른다는 것이다. 예) .env
-        return (file.indexOf('.') !== 0) && (file !== 'index.js')
+        // 해당 경로 내의 파일 중에서 .으로 시작하는 파일, index.js파일, js파일이 아닌 것은 거른다는 것이다.
+        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
     })
     .forEach(file => {
-        // sequelize 6버전에서는 let model = sequelize['import'] (path.join(__dirname, file))으로 할 경우 다음과 같은 에러가 뜬다.;
+        // sequelize 6버전에서는 let model = sequelize['import'] (path.join(__dirname, file))으로 할 경우 다음과 같은 에러가 뜬다.
         // sequelize.import is not a function
         // 아래의 코드로 수정해준다.
         let model = require(path.join(__dirname, file))(
@@ -46,7 +38,7 @@ fs.readdirSync(__dirname)
 
 Object.keys(db).forEach(modelName => {
     // 각 모델파일에서 정의한 모델명.associate = models => {}부분의 관계를 등록하는 부분으로 이해하자!
-    if("associate" in db[modelName])
+    if(db[modelName].associate)
         db[modelName].associate(db);
 })
 
